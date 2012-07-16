83eb1c18972e16687a1a65dd8103880867b7293b
Matt Beckett <beck24@gmail.com>
2012-07-16 01:51:49 -0600

    removed and/or input due to performance issues with 'and'

--- views/default/widgets/au_tagtracker/content.php
+++ views/default/widgets/au_tagtracker/content.php
@@ -8,7 +8,8 @@ $widget = $vars['entity'];
 
 if(!empty($widget->eligo_tagfilter)){
   $options = eligo_get_display_entities_options($widget);
-  $content = elgg_list_entities($options);
+  $content = elgg_list_entities_from_metadata($options);
+//  echo "<pre>" . print_r($options,1) . "</pre>";
 }
 else{
   $content = elgg_echo('au_tagtracker:enter:tags');
